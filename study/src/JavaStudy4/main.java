8bd618b879c2a473ed8dfbcd75f4a7b19497de17
Woo <fkeldh333@gmail.com>
2019-12-09 00:22:10 +0900

    JavaStudy4

--- study/src/JavaStudy4/main.java
+++ study/src/JavaStudy4/main.java
@@ -5,27 +5,28 @@ import java.util.Scanner;
 class Main {
 
 	public static void main(String[] args) {
-		int attendScore;
-		int assignmentScore;
-		int quizScore;
-		int midtermexamScore;
-		int finalexamScore;
-		double totalScore;
+		int attendScore;				//출석점수
+		int assignmentScore;			//과제점수
+		int quizScore;					//퀴즈점수
+		int midtermexamScore;			//중간점수
+		int finalexamScore;				//기말점수
+		double totalScore;				//점수의 합
 
-		char grade;
+		char grade;						//등급
 
 		Scanner scan = new Scanner(System.in);
 		System.out.print("attendance score:");
-		attendScore = scan.nextInt();
+		attendScore = scan.nextInt();			//출석점수를 받아온다
 		System.out.print("assignment score:");
-		assignmentScore = scan.nextInt();
+		assignmentScore = scan.nextInt();		//과제점수를 받아온다
 		System.out.print("quiz score:");
-		quizScore = scan.nextInt();
+		quizScore = scan.nextInt();				//퀴즈점수를 받아온다
 		System.out.print("midterm exam score:");
-		midtermexamScore = scan.nextInt();
+		midtermexamScore = scan.nextInt();		//중간점수를 받아온다
 		System.out.print("final exam score:");
-		finalexamScore = scan.nextInt();
+		finalexamScore = scan.nextInt();		//기말점수를 받아온다
 
+		//점수의 합 계산
 		totalScore = attendScore * 0.1 + assignmentScore * 0.4 + quizScore * 0.1 + midtermexamScore * 0.2
 				+ finalexamScore * 0.2;
 
