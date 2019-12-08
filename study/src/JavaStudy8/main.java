fb2c5585a8a2350dd603f810aac888732b6b4d87
Woo <fkeldh333@gmail.com>
2019-12-09 00:45:19 +0900

    JavaStudy8

--- study/src/JavaStudy8/main.java
+++ study/src/JavaStudy8/main.java
@@ -1,4 +1,4 @@
-//
+// 두 배열을 값을 크기대로 정렬하기
 
 package JavaStudy8;
 
@@ -6,13 +6,16 @@ public class main {
 	public static int[] merge(int[] arr1, int[] arr2) {
 		int index1 = 0;
 		int index2 = 0;
+		//새로운 배열의 크기설정
 		int[] merged = new int[arr1.length + arr2.length];
 
 		for (int index = 0; index < merged.length; index++) {
+			// 1번이 작으면 1번을 merged에 넣고 1++
 			if (index2 >= arr2.length || arr1[index1] < arr2[index2]) {
 				merged[index] = arr1[index1];
 				index1++;
 			} else {
+				//2번이 작으면 2번을 merged에 넣고 2++
 				merged[index] = arr2[index2];
 				index2++;
 			}
