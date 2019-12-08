2a9358ee1d6dbf82222470e3ea542252889db24c
Woo <fkeldh333@gmail.com>
2019-12-09 00:20:24 +0900

    JavaStudy3

--- study/src/JavaStudy3/Employee.java
+++ study/src/JavaStudy3/Employee.java
@@ -1,38 +1,44 @@
-package study;
+package JavaStudy3;
 
 public class Employee {
-	private String name;
-	private int number;
-	private int age;
+	private String name;		//사람 이름
+	private int number;			//사람 번호
+	private int age;			//사람 나이
 
-	public Employee() {
+	//함수를 부를 수 있게 하는 함수
+	public Employee() {			
 	}
-
-	public String getName() {
+	// 이름을 받아오는 함수
+	public String getName() {	
 		return name;
 	}
 
-	@Override
-	public String toString() {
+	//최종적인 String을 출력하는 함수
+	public String toString() {	
 		return "Employee [name=" + name + ", number=" + number + ", age=" + age + "]";
 	}
 
+	//이름을 바꾸는 함수
 	public void setName(String name) {
 		this.name = name;
 	}
 
+	//번호를 받아오는 함수
 	public int getNumber() {
 		return number;
 	}
 
+	//번호를 바꾸는 함수
 	public void setNumber(int number) {
 		this.number = number;
 	}
 
+	//나이를 받아오는 함수
 	public int getAge() {
 		return age;
 	}
 
+	//나이를 설정하는 함수
 	public void setAge(int age) {
 		this.age = age;
 	}
--- study/src/JavaStudy3/study3.java
+++ study/src/JavaStudy3/study3.java
@@ -1,9 +1,9 @@
-package study;
+package JavaStudy3;
 
 public class study3 {
 
 	public static void main(String[] args) {
-		Employee emp1 = new Employee();
+		Employee emp1 = new Employee(); // 함수 Employee 설정
 		emp1.setName("lee");
 		emp1.setNumber(100);
 		emp1.setAge(25);
