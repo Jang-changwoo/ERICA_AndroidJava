b66fc8279b0b6baa37c6687387045224db293260
Woo <fkeldh333@gmail.com>
2019-12-09 21:05:07 +0900

    JavaStudy12

--- study/src/JavaStduy12/Main.java
+++ study/src/JavaStduy12/Main.java
@@ -1,11 +1,36 @@
-//github의 오류를 고치기 위해 push하는 파일
+//String길이가 20이 넘거나 null이면 예외처리
 
 package JavaStduy12;
-// git error
+
+import java.lang.*;
+
 public class Main {
 
+	public static String len_check(String str) {
+		String result = "";
+		// 예외가 발생할 수 있는 구간
+		try {
+			// 단어길이가 20이상이면 More than 20 words 오류작성
+			if (str.length() > 20)
+				throw new RuntimeException("More than 20 words");
+			// 단어길이가 0보다 작으면(null)이면 Null오류
+			else if (str.length() < 0)
+				throw new RuntimeException("NULL");
+			else
+				result = str;
+		}
+		// try문에서 발생한 오류 처리
+		catch (RuntimeException e) {
+			e.printStackTrace();
+		}
+		return result;
+	}
+
 	public static void main(String[] args) {
-		System.out.println("hello");// TODO Auto-generated method stub
+
+		System.out.println(len_check("abcdefghijklmnopqr"));
+		System.out.println(len_check("abcdefghijklmnopqrstu"));
+		System.out.println(len_check(null));
 
 	}
 
