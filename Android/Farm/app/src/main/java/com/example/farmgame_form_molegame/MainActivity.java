62fac136c75fd7db13fdf471e66b4cc8d6ba84b5
Woo <fkeldh333@gmail.com>
2019-12-20 03:10:34 +0900

    Farm game From mole game

--- Android/Farm/app/src/main/java/com/example/farmgame_form_molegame/MainActivity.java
+++ Android/Farm/app/src/main/java/com/example/farmgame_form_molegame/MainActivity.java
@@ -1,8 +1,28 @@
 package com.example.farmgame_form_molegame;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import java.text.DecimalFormat;
+
 
 public class MainActivity extends AppCompatActivity {
 
